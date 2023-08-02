d6c3bd3fe4163e5087323d1b86a15466ef0e0899
icpar <phwoo1315@naver.com>
2023-08-02 21:51:38 +0900

    BruteForce test commit

--- greedy_practice/big_number_law.py
+++ greedy_practice/big_number_law.py
@@ -4,20 +4,20 @@ n, m, k = map(int, input().split())
 data = list(map(int, input().split()))
 
 data.sort() # 입력받은 수들 정렬하기
-first = data[n - 1] # 가장 큰수
+first = data[n - 1] # 가장 큰 수
 second = data[n - 2] # 두 번째로 큰 수
 
 result = 0
 
 while True:
-    for i in range(k):
-        if m == 0:
+    for i in range(k): # 가장 큰 수를 k번 더하기
+        if m == 0: # m이 0이라면 반복문 탈출
             break
         result += first
         m -= 1 # 더할 때마다 1씩 빼기
     if m == 0:
         break
-    result += second # 두 번째로 큰 수를 한 번 더하기
-    m -= 1 # 더할 때마다 1씩 빼기
+    result += second
+    m -= 1
 
-print(result) # 최종 답안 출력
+print(result)
