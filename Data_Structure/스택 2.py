60cbe6a8cebc6a33e30d7a675d3f46d92f4e6923
icpar <phwoo1315@naver.com>
2023-09-22 15:55:18 +0900

    Python study

---
+++ Data_Structure/스택 2.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+stack = []
+for _ in range(n):
+    a = input().rstrip()
+    if len(a) > 2:
+        stack.append(int(a[2:]))
+    elif a == '2':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack.pop())
+    elif a == '3':
+        print(len(stack))
+    elif a == '4':
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+    elif a == '5':
+        if len(stack) == 0:
+            print(-1)
+        else:
+            print(stack[-1])
