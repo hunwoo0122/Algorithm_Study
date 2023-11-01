229e7a6a9aef2d6f61609e20c2245a09a5798ddc
icpar <phwoo1315@naver.com>
2023-11-01 14:55:03 +0900

    Python study

--- bruteforce_practice/체스판 다시 칠하기.py
+++ bruteforce_practice/체스판 다시 칠하기.py
@@ -7,31 +7,30 @@ def count_repainting(board, chess):
     return count
 
 
-def main():
-    N, M = map(int, input().split())
-    board = []
-    for _ in range(N):
-        row = input()
-        board.append(row)
 
-    min_repaint = float('inf')  # 초기값을 무한대로 설정
+N, M = map(int, input().split())
+board = []
+for _ in range(N):
+    row = input()
+    board.append(row)
 
-    for i in range(N - 7):
-        for j in range(M - 7):
-            # (i, j)부터 8x8 크기의 체스판 생성
-            chess1 = [board[x][j:j + 8] for x in range(i, i + 8)]
-            chess2 = [board[x][j:j + 8] for x in range(i, i + 8)]
+min_repaint = float('inf')  # 초기값을 무한대로 설정
 
-            # 맨 왼쪽 위 칸이 흰색인 경우와 검은색인 경우에 대해 칠해야 하는 정사각형 개수를 계산하고 최솟값 갱신
-            repaint1 = count_repainting(chess1, ["WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW",
+for i in range(N - 7):
+    for j in range(M - 7):
+        # (i, j)부터 8x8 크기의 체스판 생성
+        chess1 = [board[x][j:j + 8] for x in range(i, i + 8)]
+        chess2 = [board[x][j:j + 8] for x in range(i, i + 8)]
+
+        # 맨 왼쪽 위 칸이 흰색인 경우와 검은색인 경우에 대해 칠해야 하는 정사각형 개수를 계산하고 최솟값 갱신
+        repaint1 = count_repainting(chess1, ["WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW",
                                                  "WBWBWBWB", "BWBWBWBW"])
-            repaint2 = count_repainting(chess2, ["BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB",
+        repaint2 = count_repainting(chess2, ["BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB", "BWBWBWBW", "WBWBWBWB",
                                                  "BWBWBWBW", "WBWBWBWB"])
 
-            min_repaint = min(min_repaint, repaint1, repaint2)
+        min_repaint = min(min_repaint, repaint1, repaint2)
 
-    print(min_repaint)
+print(min_repaint)
 
 
-if __name__ == "__main__":
-    main()
+
