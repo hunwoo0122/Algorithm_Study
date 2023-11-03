9c07e49838622c8301d9943a0e8c07ece546000d
icpar <phwoo1315@naver.com>
2023-11-03 11:40:00 +0900

    Python study

--- 베르트랑 공준.py
+++ 베르트랑 공준.py
@@ -6,10 +6,10 @@ while True:
     for i in range(n+1, 2*n+1): # n보다 크고 2n보다 작거나 같은 i의 수중에 소수가 있는지 탐색
         is_prime = True
         for j in range(2, int(i**0.5)+1):   # i의 소수 판별
-            if i%j == 0:
+            if i % j == 0: # i가 소수가 아닐 경우
                 is_prime = False
-                break
-        if is_prime:
+                break  # for j..문을 종료하고 계속 진행 is_prime은 False인 채로 남음
+        if is_prime:  # i가 소수일 경우만
             cnt += 1
     print(cnt)
 
--- 소수 구하기.py
+++ 소수 구하기.py
@@ -3,7 +3,7 @@ for i in range(M, N+1):
     if i==1: # 1은 소수가 아니므로 제외
         continue
     for j in range(2, int(i**0.5)+1):
-        if i%j==0:
+        if i%j==0: # 소수가 아닌 경우
             break
     else:
         print(i)
