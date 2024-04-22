e2ce0cc137e11818b4644a347351c48c2d529b37
icpar <phwoo1315@naver.com>
2024-04-22 23:10:46 +0900

    Python study

--- Dynamic Programming/피보나치 함수(재귀로 구현).py
+++ Dynamic Programming/피보나치 함수(재귀로 구현).py
@@ -2,19 +2,19 @@ import sys
 input = sys.stdin.readline
 
 T = int(input())
-def fibonacci_with_count(n, count_0, count_1):
+def fib_cnt(n, ct0, ct1):
     if n == 0:
-        count_0 += 1
-        return 0, count_0, count_1
+        ct0 += 1
+        return 0, ct0, ct1
     elif n == 1:
-        count_1 += 1
-        return 1, count_0, count_1
+        ct1 += 1
+        return 1, ct0, ct1
     else:
-        fib_n_1, count_0, count_1 = fibonacci_with_count(n - 1, count_0, count_1)
-        fib_n_2, count_0, count_1 = fibonacci_with_count(n - 2, count_0, count_1)
-        return fib_n_1 + fib_n_2, count_0, count_1
+        fib_n_1, ct0, ct1 = fib_cnt(n - 1, ct0, ct1)
+        fib_n_2, ct0, ct1 = fib_cnt(n - 2, ct0, ct1)
+        return fib_n_1 + fib_n_2, ct0, ct1
 
 for _ in range(T):
     n = int(input())
-    fib_result, f0, f1 = fibonacci_with_count(n, 0, 0)
+    fib_result, f0, f1 = fib_cnt(n, 0, 0)
     print(f0, f1)
