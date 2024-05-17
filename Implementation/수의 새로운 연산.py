87909e41105e115b393984d6e8c46c69a8c5389e
icpar <phwoo1315@naver.com>
2024-05-17 21:00:37 +0900

    Python study

--- Implementation/수의 새로운 연산.py
+++ Implementation/수의 새로운 연산.py
@@ -5,9 +5,9 @@ for i in range(1, t + 1):
     p_sum = 0
     px = 0
     py = 0
-    pDiagonal = 1
+    pDiagonal = 1        # 몇 번째 대각선인지 체크
     while True:
-        p_sum += pDiagonal
+        p_sum += pDiagonal  # p_sum은 해당 대각선에서 최댓값
         if p_sum >= p:
             px = pDiagonal - (p_sum - p)
             py = (pDiagonal + 1) - px
@@ -31,8 +31,8 @@ for i in range(1, t + 1):
     newSum = 0
     newDiagonal = newX + newY - 1
 
-    for j in range(1, newDiagonal + 1):
-        newSum += j
-    newSum -= (newDiagonal - newX)
+    for j in range(1, newDiagonal + 1): # newSum은 합쳐진 좌표의 값
+        newSum += j                # 일단 해당 대각선의 최댓값 구함
+    newSum -= (newDiagonal - newX)  #
     print("#{}".format(i), newSum, end=" ")
     print()
