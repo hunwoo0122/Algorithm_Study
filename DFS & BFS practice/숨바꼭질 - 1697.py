69023063996f912764efd049a45348135260ebe0
icpar <phwoo1315@naver.com>
2024-09-02 23:40:20 +0900

    Python study

--- DFS & BFS practice/숨바꼭질 - 1697.py
+++ DFS & BFS practice/숨바꼭질 - 1697.py
@@ -11,7 +11,10 @@ def bfs():
             print(dist[v])
             break
         for nx in (v + 1, v - 1, 2*v):
-            if (0<= nx <=MAX) and not dist[nx]: # 여기서 왜 not dist[nx]를 설정하냐면 시간이 아직 기록되지 않았기 때문
+            if (0<= nx <=MAX) and not dist[nx]: # 여기서 왜 not dist[nx]를 설정하냐면 dist[nx]가 0이 아니라는 건 전에 방문했다는
+                # 뜻이고  for문과 큐를 이용해 반복문을 돌려본결과 k에 도달하지 않는다는 것이기 때문에 굳이 한번 더 dist[nx]로
+                # 갈 필요가 없기 때문에 시간복잡도와 메모리용량을 줄일 수 있음 dist[nx] != 0 인 경우는
+                # 진작부터 탐색하기 때문(중복 탐색을 피하기 위해)
                 dist[nx] = dist[v] + 1
                 q.append(nx)
 
