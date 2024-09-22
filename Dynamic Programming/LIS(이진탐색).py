dfaa6bb9aeafa6f2700526d90255165b446c712f
icpar <phwoo1315@naver.com>
2024-09-22 20:20:52 +0900

    Python study

---
+++ Dynamic Programming/LIS(이진탐색).py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+## 이진탐색 함수
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return left
+
+lis = [arr[0]]
+for i in range(1, len(arr)):
+    if arr[i] > lis[-1]:
+        lis.append(arr[i])
+    else:
+        index = binary_search(lis, arr[i])
+        lis[index] = arr[i]
+print(len(lis))
